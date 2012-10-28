b908c35af5ddd301d578bf54409d0b8077213fa2
Reuben Cummings <reubano@gmail.com>
2012-10-28 20:46:25 +0100

    Add scripttest script

---
+++ test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+""" A script to test csv2ofx functionality """
+
+import os
+import sys
+
+from hashlib import md5
+from tempfile import NamedTemporaryFile
+from sys import exit
+from scripttest import TestFileEnvironment
+
+def main():
+	try:
+		env = TestFileEnvironment('.scripttest')
+		dir = os.path.dirname(__file__)
+		example = os.path.join('examples', 'custom_example.csv')
+		script = 'php csv2ofx.php -qm custom %s $' % example
+		result = env.run('%s' % script, cwd='%s' % dir)
+		file = NamedTemporaryFile(dir='%s' % dir, delete=False)
+		name = file.name
+		file.write('%s' % result.stdout)
+		file.close()
+		hash = md5(open(name).read()).hexdigest()
+		os.unlink(name)
+		assert hash == 'c15b2fc5fe2d0a35c4f76cb6c0297e8a'
+
+	except Exception as err:
+		sys.stderr.write('ERROR: %s\n' % str(err))
+
+	exit(0)
+
+if __name__ == '__main__': main()
