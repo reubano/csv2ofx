869283c5990421b8bcd66253b6f8f7cf44c7e505
Reuben Cummings <reubano@gmail.com>
2012-10-30 18:34:25 +0100

    Refactor script test

--- test.py
+++ test.py
@@ -14,18 +14,16 @@ def main():
 	try:
 		env = TestFileEnvironment('.scripttest')
 		dir = os.path.dirname(__file__)
+		file = NamedTemporaryFile(dir='%s' % dir, delete=False)
+		name = file.name
 
 		# test 1
-		env.run('someprogram --help')
+		env.run('csv2ofx --help')
 
 		# test 2
 		example = os.path.join('examples', 'custom_example.csv')
-		script = 'php csv2ofx.php -qm custom %s $' % example
-		result = env.run('%s' % script, cwd='%s' % dir)
-		file = NamedTemporaryFile(dir='%s' % dir, delete=False)
-		name = file.name
-		file.write('%s' % result.stdout)
-		file.close()
+		script = 'php csv2ofx.php -oqm custom %s %s' % (example, name)
+		env.run('%s' % script, cwd='%s' % dir)
 		hash = md5(open(name).read()).hexdigest()
 		os.unlink(name)
 		assert hash == 'c15b2fc5fe2d0a35c4f76cb6c0297e8a'
