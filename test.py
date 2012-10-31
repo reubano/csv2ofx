df23a5f0a9dc7b71812b0750bf5c07b024cb9ac6
Reuben Cummings <reubano@gmail.com>
2012-10-31 21:45:41 +0300

    Replace restricted variable names

--- test.py
+++ test.py
@@ -7,15 +7,18 @@ import sys
 
 from hashlib import md5
 from tempfile import NamedTemporaryFile
-from sys import exit
 from scripttest import TestFileEnvironment
 
+
 def main():
+	""" Main method
+	Returns 0 on success, 1 on failure
+	"""
 	try:
 		env = TestFileEnvironment('.scripttest')
-		dir = os.path.dirname(__file__)
-		file = NamedTemporaryFile(dir='%s' % dir, delete=False)
-		name = file.name
+		mydir = os.path.dirname(__file__)
+		myfile = NamedTemporaryFile(dir='%s' % mydir, delete=False)
+		name = myfile.name
 
 		# test 1
 		env.run('csv2ofx --help')
@@ -23,14 +26,15 @@ def main():
 		# test 2
 		example = os.path.join('examples', 'custom_example.csv')
 		script = 'php csv2ofx.php -oqm custom %s %s' % (example, name)
-		env.run('%s' % script, cwd='%s' % dir)
-		hash = md5(open(name).read()).hexdigest()
+		env.run('%s' % script, cwd='%s' % mydir)
+		myhash = md5(open(name).read()).hexdigest()
 		os.unlink(name)
-		assert hash == 'c15b2fc5fe2d0a35c4f76cb6c0297e8a'
+		assert myhash == 'c15b2fc5fe2d0a35c4f76cb6c0297e8a'
 
 	except Exception as err:
 		sys.stderr.write('ERROR: %s\n' % str(err))
 
-	exit(0)
+	sys.exit(0)
 
-if __name__ == '__main__': main()
+if __name__ == '__main__':
+	main()
