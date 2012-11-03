cca8b00fb1ab9461968a6815327442186e52fdaf
Reuben Cummings <reubano@gmail.com>
2012-11-03 21:12:57 +0100

    Rename CSV2OFX.inc.php to OFX.php

--- Autoload.php
+++ Autoload.php
@@ -4,4 +4,4 @@ require_once 'lib_general/Vars.php';
 require_once 'lib_general/File.php';
 require_once 'lib_general/String.php';
 require_once 'lib_general/MyArray.php';
-require_once 'CSV2OFX.inc.php';
+require_once 'OFX.php';
