676b3a21fe8f56e74c3b3ee4e65877eee2baae58
Reuben Cummings <reubano@gmail.com>
2015-09-13 13:47:09 +0300

    Fix unicode bug and return bytearrays

--- csv2ofx/__init__.py
+++ csv2ofx/__init__.py
@@ -37,7 +37,8 @@ __version__ = '0.16.0'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015 Reuben Cummings'
 
-md5 = lambda content: hashlib.md5(content).hexdigest()
+
+md5 = lambda content: hashlib.md5(content.encode('utf-8')).hexdigest()
 
 
 class Content(object):
