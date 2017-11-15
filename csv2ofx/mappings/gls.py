e51233571334dc8c85b79790e7e4ef99efb50710
Alper Cugun <alper@users.noreply.github.com>
2017-11-15 22:36:15 +0100

    More linter fixes

--- csv2ofx/mappings/gls.py
+++ csv2ofx/mappings/gls.py
@@ -6,15 +6,15 @@ mapping = {
     'delimiter': ';',
     'bank': 'GLS Bank',
     'account': itemgetter('Kontonummer'),
-    
+
     # Chop up the dotted German date format and put it in ridiculous M/D/Y order
-    'date': lambda r: r['Buchungstag'][3:5] + '/'
-                    + r['Buchungstag'][:2] + '/'
-                    + r['Buchungstag'][-4:],
+    'date': lambda r: 
+        r['Buchungstag'][3:5] + '/' + r['Buchungstag'][:2] + '/' +
+        r['Buchungstag'][-4:],
 
     # locale.atof does not actually know how to deal with German separators.
     # So we do it the crude way
-    'amount': lambda r: r['Betrag'].replace('.', '').replace(',', '.'), 
+    'amount': lambda r: r['Betrag'].replace('.', '').replace(',', '.'),
     'desc': itemgetter('Buchungstext'),
     'payee': itemgetter('Auftraggeber/Empfänger'),
 }
