0d979a61a98cc5b24d2a0ae3f776f2f70cd6e64a
Steve Short <steve@shortcentral.com>
2017-12-03 13:55:55 +0000

    Added Money Dashboard mapping - mdb

---
+++ csv2ofx/mappings/mdb.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# vim: sw=4:ts=4:expandtab
+# pylint: disable=invalid-name
+"""
+csv2ofx.mappings.mdb
+~~~~~~~~~~~~~~~~~~~~
+
+Provides a mapping for transactions obtained via moneydashboard.com
+"""
+from __future__ import absolute_import
+
+from operator import itemgetter
+
+mapping = {
+    'is_split': False,
+    'has_header': True,
+    'currency': 'GBP',
+    'account': itemgetter('Account'),
+    'date': itemgetter('Date'),
+    'amount': itemgetter('Amount'),
+    'desc': itemgetter('OriginalDescription'),
+    'payee': itemgetter('CurrentDescription'),
+    'class': itemgetter('Tag'),
+}
