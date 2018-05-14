b416addd53a2b779f9ee5a311217bdf3a82ce9b5
ak2k <akirby@gmail.com>
2018-05-14 10:42:54 -0400

    Update mintapi.py

--- csv2ofx/mappings/mintapi.py
+++ csv2ofx/mappings/mintapi.py
@@ -27,6 +27,6 @@ mapping = {
     'bank': itemgetter('fi'),
     'currency': 'USD',
     'id': itemgetter('id'),
-    'shares': lambda tr: tr['shares'] if tr['shares'] is not '' else 0.0,
+    'shares': lambda tr: tr['shares'] if tr['shares'] != '' else 0.0,
     'symbol': itemgetter('symbol'),
 }
