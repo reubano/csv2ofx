26dce82b916525ac508531e0365fc21bccb576e5
Reuben Cummings <reubano@gmail.com>
2018-06-27 16:59:20 -0500

    Add GLS VWZ fields back as ‘notes’

--- csv2ofx/mappings/gls.py
+++ csv2ofx/mappings/gls.py
@@ -24,5 +24,6 @@ mapping = {
     # So we do it the crude way
     'amount': lambda r: r['Betrag'].replace('.', '').replace(',', '.'),
     'desc': itemgetter('Buchungstext'),
+    'notes': lambda r: ' '.join(r['VWZ-{}'.format(n)] for n in range(1, 15)),
     'payee': itemgetter('Auftraggeber/Empfänger'),
 }
