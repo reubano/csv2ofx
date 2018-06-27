03e88ac5bec6664bb1b04a8b0a3c37011bf22fdf
Reuben Cummings <reubano@gmail.com>
2018-06-27 16:27:03 -0500

    Get GLS test working

--- csv2ofx/mappings/gls.py
+++ csv2ofx/mappings/gls.py
@@ -19,8 +19,7 @@ mapping = {
     # locale.atof does not actually know how to deal with German separators.
     # So we do it the crude way
     'amount': lambda r: r['Betrag'].replace('.', '').replace(',', '.'),
-    'type': itemgetter('Buchungstext'),
-    'desc': lambda r: ' '.join([r['VWZ1'], r['VWZ2'], r['VWZ3'], r['VWZ4'], r['VWZ5'], r['VWZ6'], r['VWZ7'], r['VWZ8'], r['VWZ9'], r['VWZ10'], r['VWZ11'], r['VWZ12'], r['VWZ13'], r['VWZ14']]),
     # Unicode marker required for python2.7
     'payee': itemgetter(u'Auftraggeber/Empfänger'),
+    'desc': itemgetter('Buchungstext'),
 }
