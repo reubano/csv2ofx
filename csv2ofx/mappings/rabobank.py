72046d8abbaca77ce61a4c780d5d0b9a2a193be4
Angelo Hongens <angelo@hongens.nl>
2020-06-29 20:09:22 -0400

    update rabobank mapping to reflect the current CSV files

--- csv2ofx/mappings/rabobank.py
+++ csv2ofx/mappings/rabobank.py
@@ -1,15 +1,15 @@
 # coding: utf-8
-
 from __future__ import (
     absolute_import, division, print_function, unicode_literals)
-
 from operator import itemgetter
 
+# example to convert:
+# csv2ofx -m rabobank -E ISO-8859-1 CSV_O_20200630_014400.csv CSV_O_20200630_014400.ofx
 
 def date_func(trxn):
     tag = trxn['Datum']
+    tag = tag.split('-')
 
-    # Chop up the ISO date and put it in ridiculous M/D/Y order
     return '{}/{}/{}'.format(tag[1], tag[2], tag[0])
 
 mapping = {
@@ -21,7 +21,7 @@ mapping = {
     'id': itemgetter('Volgnr'),
     'date': date_func,
     'amount': lambda r: r['Bedrag'].replace(',', '.'),
-    'desc': lambda r: ' '.join(
-        r['Omschrijving-{}'.format(n)] for n in range(1, 4)),
+    'desc': lambda r: '{0} - {1}'.format(r['Naam tegenpartij'],
+                                         ' '.join(r['Omschrijving-{}'.format(n)] for n in range(1, 4))),
     'payee': itemgetter('Naam tegenpartij'),
 }
