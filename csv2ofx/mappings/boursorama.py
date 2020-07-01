9b4b5b4b934a5516a0056a2ef0cf2659cecbd928
Brice Ahouangonou <brice.ahouangonou@gmail.com>
2020-07-01 23:57:47 +0200

    New mapping for Boursorama Banque (fr)

---
+++ csv2ofx/mappings/boursorama.py
@@ -0,0 +1,21 @@
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals)
+
+from operator import itemgetter
+
+
+mapping = {
+    'has_header': True,
+    'is_split': False,
+    'delimiter': ';',
+    'bank': 'Boursorama',
+    'account': itemgetter('accountNum'),
+    'account_id': itemgetter('accountNum'),
+    'date': itemgetter('dateOp'),
+    'type': 'checking',
+    'amount': lambda r: r['amount'].replace(' ', '').replace(',', '.'),
+    'currency': 'EUR',
+    'desc': itemgetter('label'),
+    'date_fmt': '%Y%Y-%m%m-%d%d',
+    'balance': itemgetter('accountbalance'),
+}
