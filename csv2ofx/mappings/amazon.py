4cd5884d0cc77975387e6c403305b0471904a151
Jason R. Coombs <jaraco@jaraco.com>
2023-06-25 14:20:15 -0400

    Add support for filtering payment cards by inclusion in addition to exclusion.

--- csv2ofx/mappings/amazon.py
+++ csv2ofx/mappings/amazon.py
@@ -3,10 +3,14 @@ Import transactions from Amazon Order History
 as exported by Amazon Order History Reporter
 (https://chrome.google.com/webstore/detail/amazon-order-history-repo/mgkilgclilajckgnedgjgnfdokkgnibi).
 
-Honors a couple of environment variables:
+Honors some environment variables:
 
+ - ``AMAZON_INCLUDE_CARDS``: comma-separated last-four digits
+   of cards or payment methods to include in the output. If not
+   supplied, defaults to all cards.
  - ``AMAZON_EXCLUDE_CARDS``: comma-separated last-four digits
-   of cards or payment methods to exclude in the output.
+   of cards or payment methods to exclude in the output (supersedes
+   include).
  - ``AMAZON_PURCHASES_ACCOUNT``: The OFX "account id" to use.
    Financial tools will use this account ID to associated with an
    account. If unspecified, defaults to "100000001".
@@ -17,14 +21,28 @@ import functools
 from operator import itemgetter
 
 
+All = ['']
+"""
+A special value matching all cards.
+"""
+
+
 @functools.lru_cache()
-def cards():
+def exclude_cards():
     setting = os.environ.get('AMAZON_EXCLUDE_CARDS', None)
     return setting.split(',') if setting else []
 
 
-def exclude_cards(row):
-    return not any(card in row['payments'] for card in cards())
+@functools.lru_cache()
+def include_cards():
+    setting = os.environ.get('AMAZON_INCLUDE_CARDS', None)
+    return setting.split(',') if setting else All
+
+
+def filter_payment(row):
+    include = any(card in row['payments'] for card in include_cards())
+    exclude = any(card in row['payments'] for card in exclude_cards())
+    return include and not exclude
 
 
 mapping = {
@@ -39,5 +57,5 @@ mapping = {
     'id': itemgetter('order id'),
     'type': 'DEBIT',
     'last_row': -1,
-    'filter': exclude_cards,
+    'filter': filter_payment,
 }
