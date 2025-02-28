badc0a6e0232eef062be2adce89bf2ede0fdb52d
Jason R. Coombs <jaraco@jaraco.com>
2025-02-28 17:24:24 -0500

    Disable the ubs-ch-fr module in GitHub Linux.

--- conftest.py
+++ conftest.py
@@ -1,3 +1,13 @@
+import os
+import platform
+
+
+is_GitHub_Linux = bool(
+    os.environ.get("GITHUB_ACTIONS") and platform.system() == "Linux"
+)
+
 collect_ignore = [
     "csv2ofx/main.py",  # if imported, fails on parse_args
-]
+] + [
+    "csv2ofx/mappings/ubs-ch-fr.py",
+] * is_GitHub_Linux
